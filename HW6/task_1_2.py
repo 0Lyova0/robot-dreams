7f5f85ea258f0b31420c002d6890cfdd50fd4083
0Lyova0 <levkulychstepan@gmail.com>
2023-04-09 16:29:28 +0200

    changes are made

--- HW6/task_1_2.py
+++ HW6/task_1_2.py
@@ -1,10 +1,17 @@
-def add_numbers(a, b):
-    return a ** b
+# I don't know how to_do 2 ** 0.5
+# now I have 2 ** 2 and 0.5 ** 0.5
+# How can I take a second arg?
 
-exp_numbers = add_numbers(2, 3)
-print(exp_numbers)
+def add_numbers(*args):
+    for arg in args:
+        print(arg ** arg)
+
+add_numbers(4, 0.5)
 
 
-def add_numbers2(a=2, b=5):
-    return  a + b
-print(add_numbers2())
+
+def add_numbers2(*args):
+    print(sum(args))
+
+add_numbers2(1,2,3,4,5)
+
